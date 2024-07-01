f7e4c2a72b159d020b83e8bf19c191bb3660cac6
Fluffycoal <kiptoo.brian@strathmore.edu>
2024-07-01 09:45:34 +0300

    converted html to  php

---
+++ about.php
@@ -0,0 +1,29 @@
+
+    
+    <p style="text-align: justify;">
+        We at Pastry Bakery add in a home made touch in the fine taste of pastries, breads, cakes, tarts that is baked fresh daily, complimented by freshly brewed Kenyan coffee, or our wide range of special hot or iced range of beverages, all made with love.<br>
+    </p>
+    <p>
+        Authentically handmade, using recipes passed down through generations as well as keeping up with the trend, we are innovative and try and test new recipes and flavours.<br>
+    </p>
+    <p> 
+        We love the art, the tradition and the skill that goes into creating & baking our range of breads, traditional pastries, tarts, signature and custom, layered cakes for all occasions, as well as cheesecakes.<br>
+    </p>
+    <p>  
+        We use fresh, natural ingredients, and we pride ourselves on not adding any additives or preservatives in our baked goods.<br>
+    </p>
+    <p>    
+        The skill of our bakers and the time we patiently take to create the perfect treat that gets everyone talking.<br>
+    </p>
+    <p> 
+        We value our customers and no order is too big or small, we will be more than happy to take time to take your order in-store or online, our customer service team will take care of your needs, and the bakers will be ready to whisk & bake your dream come true.<br>
+    </p>
+    <p> 
+        Our team is always full of love and laughter. That is our moto too “baked with love and laughter” because that is what the true meaning of “Tiramisu” actually means in Italian…..
+    </p>
+    <footer style="text-align:center;">     
+        “To cheer you up” or to “lift up your spirits.”
+    </footer>
+    
+</body>
+</html>
