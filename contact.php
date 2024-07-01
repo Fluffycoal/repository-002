3d79d20b2b599d305790e356da082bd2e25601ed
Fluffycoal <kiptoo.brian@strathmore.edu>
2024-07-01 09:23:34 +0300

    converted into php

---
+++ contact.php
@@ -0,0 +1,23 @@
+
+
+
+<div class="container">
+    <form>
+    <h1 style = "text-align:center;
+    color: rgb(51, 48, 50);
+    text-transform: uppercase;
+    text-decoration: wavy;
+    line-height: 5px;">Contact Us</h1>
+    <input type="text" id="firstname" placeholder="First Name" required>
+    <input type="text" id="lasttname" placeholder="last Name" required>
+    <input type="email" id="firstname" placeholder="Email" required>
+    <input type="text" id="mobile" placeholder="Mobile" required>
+    <h4>Type Your Message Here.....</h4>
+    <textarea required></textarea>
+    <input type="submit" value="send" id="button"/>
+</form>
+</div>
+
+    
+</body>
+</html>
