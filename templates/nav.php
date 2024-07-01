1646a8c5f3fe49d149cb23d9bdfa33538f8268a0
Fluffycoal <kiptoo.brian@strathmore.edu>
2024-07-01 09:19:02 +0300

    added details in nav php

---
+++ templates/nav.php
@@ -0,0 +1,10 @@
+<div class="topnav">
+    <a href="./">Home</a>
+    <a href="about.html">About us</a>
+    <a href="menu.html">Menu</a>
+    <a href="contact.html">Contact Us</a>
+    
+    <div class="topnav-right">
+        <a href="myaccount.html">Sign Up</a>
+    </div>
+</div>
