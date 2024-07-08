9070f5b59ecbd5850024c92245592c5bf2b34539
Fluffycoal <kiptoo.brian@strathmore.edu>
2024-07-08 15:12:14 +0300

    added insert code for the database

--- Register.php
+++ Register.php
@@ -1,41 +1,54 @@
 <?php
+    include_once("includes/db_connect.php");
     include_once("templates/header.php");
     include_once("templates/nav.php");
 
+    if(isset($_POST["sign_up"])){
+        $username =mysqli_real_escape_string($conn, addslashes( $_POST["username"]));
+        $password = mysqli_real_escape_string($conn, addslashes($_POST["password"]));
+        $email = mysqli_real_escape_string($conn, addslashes($_POST["email"]));
+        $mobile = mysqli_real_escape_string($conn, addslashes($_POST["mobile"]));
+        
+
+        $insert_user = "INSERT INTO user(username, password, email,mobile  )
+        VALUES ('$username', '$password', '$email', '$mobile')";
+
+if ($conn->query($insert_user) === TRUE) {
+    echo "New record inserted successfully";
+    header("Location: Register.php");
+    exit();
+  } else {
+    echo "Error: " . $insert_user. "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+  }
 ?>
 
 <body>
     <center>
         <h3>Register Here</h3>
-        <form action="" method="">
-            <table>
+        <form action="" method="post">
+            <table3>
                 <tr>
                     <td>Username: </td>
                     <td>
-                        <input type="text" name="user" placeholder="Enter name Here">
+                        <input type="text" name="username" placeholder="Enter name Here">
                     </td>
                 </tr>
                 <tr>
                     <td>Password:</td>
                     <td>
-                        <input type="password" name="pass" placeholder="Enter password here">
+                        <input type="password" name="password" placeholder="Enter password here">
                     </td>
                 </tr>
                 <tr>
                     <td>Email:</td>
                     <td>
-                        <input type="email" name="mail" placeholder="Enter email here">
+                        <input type="email" name="email" placeholder="Enter email here">
                     </td>
                 </tr>
-                <tr>
-                    <td>Gender:</td>
-                    <td>
-                        <input type="radio" name="gender"> Male
-                        <input type="radio" name="gender">Female
-                        <input type="radio" name="others">Others
-                    </td>
-                </tr>
-                <tr>
+                
                     <td>Phone:</td>
                     <td>
                         <select>
@@ -48,7 +61,7 @@
                 </tr>
                 <tr>
                     <td>
-                        <input type="submit" name="submit" value="submit">
+                        <input type="submit" name="sign_up" value="Sign Up">
                     </td>
                     <td>
                         Already a Member?<a href="myaccount.php">Login Here</a>
